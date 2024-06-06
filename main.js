a88243de2575834c9d947ad98c92545307d52207
iqrahassan904 <iqrasher142@gmail.com>
2024-06-06 17:31:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.92,
+    GPB: 0.79,
+    INR: 83.28,
+    PKR: 278,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "enter from amount",
+        type: "list",
+        choices: ["USD", "EUR", "GPB", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: "enter to amount",
+        type: "list",
+        choices: ["USD", "EUR", "GPB", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "enter tO amount",
+        type: "number",
+    },
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
